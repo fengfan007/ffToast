c4514d207c6a93babfa1aca64083aee6d2f1b988
fengfan <18392069657@163.com>
2021-02-04 00:04:21 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+
+    <button @click="handleClick">点击触发toast</button>
+
+    <test-toast ref="toast"></test-toast>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    handleClick() {
+      this.$nextTick( () => {
+        this.$refs.toast.toastPlugin('哎呀，我被触发了！' , 3000)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/plugin/toast/lib/toast.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="toast"  ref='toastPosition' :class="{active: toastHidden}">
+      <div class="toast-warpper">
+        {{text}}
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'test-toast',
+  data () {
+    return {
+      text: '', //提示内容
+      toastHidden: false    //隐藏or展示
+    }
+  },
+  created () {
+    // this.toastPlugin()
+  },
+  methods: {
+    toastPlugin (msg, time) {
+      this.text = msg
+      this.toastHidden = true
+      setTimeout(() => {
+        this.toastHidden = false
+      }, time)
+    }
+  }
+}
+</script>
+ 
+<style>
+  .toast {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 0px;
+    padding: 20px;
+    min-height: 0px;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+    color: #fff;
+    transition: all 0.5s;
+    z-index: -1;
+    opacity: 0;
+  }
+  .toast.active {
+    width: 150px;
+    min-height: 25px;
+    opacity: 1;
+    z-index: 11;
+    animation: upDown .7s linear ;
+  }
+  @keyframes upDown{
+    0%{ top: 0;}
+    50%{ top: 50%;}
+    70%{ top: 40%;}
+    100%{ top: 50%;}
+ }
+</style>
